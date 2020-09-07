1a65807747c33eb035e5df68fd0ae920e5260c76
Bode <bode.falade@outlook.com>
2020-09-06 22:23:52 -0500

    edited the Migrate package.

--- cmd/migrations/migrations.go
+++ cmd/migrations/migrations.go
@@ -2,24 +2,23 @@ package migrations
 
 import (
 	"github.com/bodecci/fintech-bank-app/cmd/helpers"
-	"github.com/bodecci/fintech-app/cmd/helpers"
-	"github.com/jinzhu/gorm"
-	"github.com/jinzhu/gorm/dialects/postgres"
+	gorm "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type User struct {
 	gorm.Model
 	Username string
-	Email 	 string
+	Email    string
 	Password string
 }
 
 type Account struct {
 	gorm.Model
-	Type string
-	Name string
+	Type    string
+	Name    string
 	Balance uint
-	UserID uint
+	UserID  uint
 }
 
 func connectDB() *gorm.DB {
@@ -36,18 +35,27 @@ func createAccounts() {
 		{Username: "Mama", Email: "mama@mama.com"},
 	}
 
-	for i:= 0; i < len(users); i++ {
+	for i := 0; i < len(users); i++ {
 		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
-		user := User{Username: users[i].Username, Email:user[i].Email, Password: generatedPassword}
+		user := User{
+			Username: users[i].Username,
+			Email:    users[i].Email,
+			Password: generatedPassword}
 		db.Create(&user)
+
+		account := Account{
+			Type:    "Checking Account",
+			Name:    string(users[i].Username + "'s" + "account"),
+			Balance: uint(10000 * int(i+1)), UserID: user.ID}
+		db.Create(&account)
 	}
 
-	defer db.Close
+	defer db.Close()
 }
 
 func Migrate() {
 	db := connectDB()
-	db.AutoMigrate(&Users{}, &Account{})
+	db.AutoMigrate(&User{}, &Account{})
 	defer db.Close()
 
 	createAccounts()
