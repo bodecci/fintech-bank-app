157ca526b576ffa090795c0d08606ed084e4ee8a
Bode <bode.falade@outlook.com>
2020-09-06 23:01:15 -0500

    refactored account and user creation.

--- cmd/migrations/migrations.go
+++ cmd/migrations/migrations.go
@@ -2,24 +2,10 @@ package migrations
 
 import (
 	"github.com/bodecci/fintech-bank-app/cmd/helpers"
+	"github.com/bodecci/fintech-bank-app/interfaces"
 	gorm "github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
-
-type User struct {
-	gorm.Model
-	Username string
-	Email    string
-	Password string
-}
-
-type Account struct {
-	gorm.Model
-	Type    string
-	Name    string
-	Balance uint
-	UserID  uint
-}
 
 func connectDB() *gorm.DB {
 	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=user dbname=dbname password=password sslmode=disable")
@@ -30,20 +16,20 @@ func connectDB() *gorm.DB {
 func createAccounts() {
 	db := connectDB()
 
-	users := [2]User{
+	users := &[2]interfaces.User{
 		{Username: "Bodmas", Email: "bodmas@bodmas.com"},
 		{Username: "Mama", Email: "mama@mama.com"},
 	}
 
 	for i := 0; i < len(users); i++ {
 		generatedPassword := helpers.HashAndSalt([]byte(users[i].Username))
-		user := User{
+		user := &interfaces.User{
 			Username: users[i].Username,
 			Email:    users[i].Email,
 			Password: generatedPassword}
 		db.Create(&user)
 
-		account := Account{
+		account := &interfaces.Account{
 			Type:    "Checking Account",
 			Name:    string(users[i].Username + "'s" + "account"),
 			Balance: uint(10000 * int(i+1)), UserID: user.ID}
@@ -54,8 +40,10 @@ func createAccounts() {
 }
 
 func Migrate() {
+	User := &interfaces.User{}
+	Account := &interfaces.Account{}
 	db := connectDB()
-	db.AutoMigrate(&User{}, &Account{})
+	db.AutoMigrate(&User, &Account)
 	defer db.Close()
 
 	createAccounts()
