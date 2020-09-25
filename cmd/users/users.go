e2e4c7869ca37797f1f676b759fd55d3e828dea8
Bode <bode.falade@outlook.com>
2020-09-24 22:09:11 -0500

    created users package for user login.

--- cmd/users/users.go
+++ cmd/users/users.go
@@ -1,8 +1,11 @@
 package users
 
 import (
+	"time"
+
 	"github.com/bodecci/fintech-bank-app/cmd/helpers"
 	interfaces "github.com/bodecci/fintech-bank-app/cmd/interfaces"
+	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -11,6 +14,7 @@ func Login(username string, pass string) map[string]interface{} {
 	db := helpers.ConnectDB()
 	user := &interfaces.User{}
 
+	// look for the user with username passed in from the function params
 	if db.Where("username = ? ", username).First(&user).RecordNotFound() {
 		return map[string]interface{}{"message": "User not found"}
 	}
@@ -21,5 +25,32 @@ func Login(username string, pass string) map[string]interface{} {
 	if passErr != nil && passErr == bcrypt.ErrMismatchedHashAndPassword {
 		return map[string]interface{}{"message": "Wrong password"}
 	}
+
+	accounts := []interfaces.ResponseAccount{}
+	db.Table("accounts").Select("id, name, balance").Where("user_id = ? ", user.ID).Scan(&accounts)
+
+	responseUser := &interfaces.ResponseUser{
+		ID:       user.ID,
+		Username: user.Username,
+		Email:    user.Email,
+		Accounts: accounts,
+	}
+
+	defer db.Close()
+
+	tokenContent := jwt.MapClaims{
+		"user_id": user.ID,
+		"expiry":  time.Now().Add(time.Minute * 60).Unix,
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenContent)
+	token, err := jwtToken.SignedString([]byte("TokenPassword"))
+	helpers.HandleErr(err)
+
+	var response = map[string]interface{}{"message": "all is fine"}
+	response["jwt"] = token
+	response["data"] = responseUser
+
+	return response
 
 }
