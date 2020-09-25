8fba83b3b2ff8a166a7c0ed83a4ada212812ce65
Bode <bode.falade@outlook.com>
2020-09-25 14:56:11 -0500

    added api startup to main.

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"github.com/bodecci/fintech-bank-app/cmd/migrations"
+	"github.com/bodecci/fintech-bank-app/cmd/api"
 )
 
 func main() {
-	migrations.Migrate()
+	//migrations.Migrate()
+	api.StartApi()
 
 }
